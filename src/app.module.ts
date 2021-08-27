7ef63f01bb8c7f617c8741795d277e85ce27aa29
Alexis Gómez <00122717@uca.edu.sv>
2021-08-26 19:24:21 -0600

    :sparkles: Add typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'process';
 import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mongodb',
+          autoLoadEntities: true,
+          synchronize: true,
+          url: configService.get('DB_URL'),
+          useUnifiedTopology: true,
+        };
+      },
+    }),
+    BookModule,
+  ],
 })
 export class AppModule {}
