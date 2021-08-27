b27b98aacd55da9632e07800348b397ac577325a
Alexis Gómez <00122717@uca.edu.sv>
2021-08-27 10:59:36 -0600

    :rocket: Change deployment config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from 'process';
 import { BookModule } from './book/book.module';
 import { AuthModule } from './auth/auth.module';
 import { RentalModule } from './rental/rental.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: [`.env.stage.${process.env.STAGE}`],
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          type: 'mongodb',
-          autoLoadEntities: true,
-          synchronize: true,
-          url: configService.get('DB_URL'),
-          useUnifiedTopology: true,
-        };
-      },
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      autoLoadEntities: true,
+      synchronize: true,
+      url: process.env.DB_URL,
+      useUnifiedTopology: true,
     }),
     BookModule,
     AuthModule,
