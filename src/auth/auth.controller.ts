6aee52dd5eeb534f2e17f5961bbe0efdf7111233
Alexis Gómez <00122717@uca.edu.sv>
2021-08-26 21:12:21 -0600

    :sparkles: Add create user functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { create } from 'eslint/lib/rules/*';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/user')
+  addUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.authService.createUser(createUserDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { resolveSoa } from 'dns';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { password, role, ...userDetails } = createUserDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    try {
+      const user = await this.userRepository.save({
+        ...userDetails,
+        password: hashedPassword,
+        roles: [role],
+      });
+      return user;
+    } catch (error) {
+      if ((error.code = 11000)) {
+        throw new BadRequestException('A user with that email already exists');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+}
