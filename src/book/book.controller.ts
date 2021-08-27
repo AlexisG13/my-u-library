db385be96b343f16d3c2f64e14e87744f0b2a79b
Alexis Gómez <00122717@uca.edu.sv>
2021-08-26 19:36:23 -0600

    :sparkles: Add book functionality

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { CreateBookDto } from './dtos/create-book.dto';
+import { Book } from './entities/book.entity';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Post()
+  addBook(@Body() createBookDto: CreateBookDto): Promise<Book> {
+    return this.bookService.addBook(createBookDto);
+  }
+}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateBookDto } from './dtos/create-book.dto';
+import { Book } from './entities/book.entity';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectRepository(Book) private bookRepository: Repository<Book>,
+  ) {}
+
+  async addBook(createBookDto: CreateBookDto): Promise<Book> {
+    return this.bookRepository.save(createBookDto);
+  }
+}
